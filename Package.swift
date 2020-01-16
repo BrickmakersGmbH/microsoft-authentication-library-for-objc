acb105295e06dfe3a14cc38ec3d557d5fe276dfd
Pascal Friedrich <pascal.friedrich@brickmakers.de>
2020-01-16 14:23:44 +0100

    Change Path of Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "microsoft-authentication-library-for-objc",
+    name: "MSAL",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "microsoft-authentication-library-for-objc",
-            targets: ["microsoft-authentication-library-for-objc"]),
+            name: "MSAL",
+            targets: ["MSAL"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "microsoft-authentication-library-for-objc",
-            dependencies: []),
+            name: "MSAL",
+            dependencies: [],
+            path: "MSAL"),
         .testTarget(
             name: "microsoft-authentication-library-for-objcTests",
-            dependencies: ["microsoft-authentication-library-for-objc"]),
+            dependencies: ["MSAL"]),
     ]
 )
